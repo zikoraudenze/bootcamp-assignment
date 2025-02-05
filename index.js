2f39322ef8e4d27e62dabb07827c23a52d352777
Kingsley Udenze <zikoraudenze@gmail.com>
2025-02-04 21:52:05 -0600

    added files for assignment

---
+++ index.js
@@ -0,0 +1,76 @@
+
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your application:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error('Error writing the file:', err);
+      } else {
+        console.log('README.md file has been generated successfully!');
+      }
+    });
+  }
+  
+  // TODO: Create a function to initialize app
+  function init() {
+    inquirer.prompt(questions).then((answers) => {
+      const readmeContent = generateReadme(answers);
+      writeToFile('README.md', readmeContent);
+    });
+  }
+  
+  // Function call to initialize app
+  init();
