97658a03cb0a15a80a506f0f716d7bbf20aca514
Kingsley Udenze <zikoraudenze@gmail.com>
2025-02-04 23:14:57 -0600

    updated readme and index file

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# bootcamp-assignment
+# READme generator
+  
+  ![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)
+  
+  ## Description
+  This app creates a professional readme
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  run node index.js
+  
+  ## Usage
+  instructions can be found in the readme
+  
+  ## License
+This project is licensed under the GPL 3.0 license.
+  
+  ## Contributing
+  clone the repo and request contributor access
+  
+  ## Tests
+  test app on local machine before pull request is raised
+  
+  ## Questions
+  If you have any questions, you can reach me at:
+  - GitHub: [zikoraudenze](https://github.com/zikoraudenze)
+  - Email: zikoraudenze@gmail.com
+  
--- index.js
+++ index.js
@@ -53,6 +53,55 @@ const questions = [
     },
   ];
 
+  const licenseBadges = {
+    'MIT': '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'Apache 2.0': '![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)',
+    'GPL 3.0': '![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    'BSD 3-Clause': '![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)',
+  };
+  
+  function generateReadme(data) {
+    const licenseBadge = licenseBadges[data.license] || '';
+    const licenseSection = data.license !== 'None'
+      ? `## License\nThis project is licensed under the ${data.license} license.`
+      : '';
+  
+    return `# ${data.title}
+  
+  ${licenseBadge}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ${licenseSection}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions, you can reach me at:
+  - GitHub: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email}
+  `;
+  }
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
